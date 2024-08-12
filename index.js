43744f04cc99f60b8ae2bd7ccffbcde823d45faf
Bernardo Cerqueira <katanardo@gmail.com>
2024-08-12 20:31:23 -0300

    Fixed Edit Button Function

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ function createTransactionContainer(id) {
         transactions.splice(indexToRemove, 1, transaction)
         document.querySelector(`#transaction-${id}`).remove()
         renderTransaction(transaction)
+        document.querySelector('#id').value = ''
     
     }else{
         const response = await fetch('http://localhost:3000/transactions', {
@@ -118,43 +119,6 @@ function createEditTransactionBtn(transaction) {
       document.querySelector('#amount').value = transaction.amount
     })
     return editBtn
-  }
-
-  async function saveTransaction(ev) {
-    ev.preventDefault()
-  
-    const id = document.querySelector('#id').value
-    const name = document.querySelector('#name').value
-    const amount = parseFloat(document.querySelector('#amount').value)
-  
-    if (id) {
-      const response = await fetch(`http://localhost:3000/transactions/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ name, amount }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const transaction = await response.json()
-      const indexToRemove = transactions.findIndex((t) => t.id === id)
-      transactions.splice(indexToRemove, 1, transaction)
-      document.querySelector(`#transaction-${id}`).remove()
-      renderTransaction(transaction)
-    } else {
-      const response = await fetch('http://localhost:3000/transactions', {
-        method: 'POST',
-        body: JSON.stringify({ name, amount }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const transaction = await response.json()
-      transactions.push(transaction)
-      renderTransaction(transaction)
-    }
-  
-    ev.target.reset()
-    updateBalance()
   }
 
   function createDeleteTransactionButton(id) {
